47c9c530f23d61c9afd0565c6702194beb3e6c57
Brendon Alexander <alexander.brendon2002@gmail.com>
2026-05-22 16:33:36 -0300

    Cenario organizado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.U2D;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-
     private Vector3 target;
+    public Animator animator;
 
     void Start()
     {
         target = transform.position;
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
+    {
+        Movimento();
+
+    }
+
+    private void Movimento()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
@@ -20,10 +28,8 @@ public class PlayerMovement : MonoBehaviour
             target.z = transform.position.z;
         }
 
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            target,
-            speed * Time.deltaTime
-        );
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
 }
+
+
