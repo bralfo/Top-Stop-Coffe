314f8500e5e26cd041d60a56d65c4fd259f094d4
Brendon Alexander <alexander.brendon2002@gmail.com>
2026-05-26 13:57:20 -0300

    move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.U2D;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     private Vector3 target;
     public Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private bool isMoving;
 
     void Start()
     {
         target = transform.position;
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         Movimento();
-
+        UpdateAnimation();
     }
 
     private void Movimento()
@@ -30,6 +32,22 @@ public class PlayerMovement : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
+
+    private void UpdateAnimation()
+    {
+
+        isMoving = Vector3.Distance(transform.position, target) > 0.01f;
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
+        {
+            Vector3 direction = (target - transform.position).normalized;
+            if (spriteRenderer != null && Mathf.Abs(direction.x) > 0.1f)
+            {
+                spriteRenderer.flipX = direction.x < 0;
+            }
+        }
+
+
+    }
 }
-
-
