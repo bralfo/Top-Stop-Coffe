18829a329377bdff56a069dd0f636df7b44543cd
Brendon Alexander <alexander.brendon2002@gmail.com>
2026-05-27 15:18:39 -0300

    Tilemap rewind

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,37 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+
     private Vector3 target;
+    private Rigidbody2D rb;
+
     public Animator animator;
     private SpriteRenderer spriteRenderer;
+
     private bool isMoving;
 
     void Start()
     {
         target = transform.position;
+
+        rb = GetComponent<Rigidbody2D>();
+
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        Movimento();
+        HandleInput();
         UpdateAnimation();
     }
 
-    private void Movimento()
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void HandleInput()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
@@ -38,28 +50,33 @@ public class PlayerMovement : MonoBehaviour
                 transform.position.z
             );
         }
-
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            target,
-            speed * Time.deltaTime
-        );
     }
+
+    private void MovePlayer()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(
+            rb.position,
+            target,
+            speed * Time.fixedDeltaTime
+        );
+
+        rb.MovePosition(newPosition);
+    }
+
     private void UpdateAnimation()
     {
+        isMoving = Vector2.Distance(rb.position, target) > 0.05f;
 
-        isMoving = Vector3.Distance(transform.position, target) > 0.01f;
         animator.SetBool("isMoving", isMoving);
 
         if (isMoving)
         {
             Vector3 direction = (target - transform.position).normalized;
+
             if (spriteRenderer != null && Mathf.Abs(direction.x) > 0.1f)
             {
                 spriteRenderer.flipX = direction.x < 0;
             }
         }
-
-
     }
 }
